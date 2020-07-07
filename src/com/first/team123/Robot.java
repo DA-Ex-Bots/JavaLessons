3802aa6a292e436149c7efed81d29f627130c9d4
Richard Snyder <firstwatchwebs@hotmail.com>
2020-07-07 13:16:53 -0500

    Add initial code.

---
+++ src/com/first/team123/Robot.java
@@ -0,0 +1,28 @@
+package com.first.team123;
+
+public class Robot {
+
+    Robot(int numberMotors) {
+        this.numberMotors = numberMotors;
+    }
+
+    private int numberMotors = 4;
+    private double defaultPower = 0.85;
+
+
+    void init() {
+
+    }
+
+    void Straight(int inches, double power) {
+
+    }
+
+    void Spin(int degrees, double power) {
+
+    }
+
+    void Turn(int degrees, double radius, double power) {
+
+    }
+}
