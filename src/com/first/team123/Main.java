e2ffa1a4d5ee04dec0e803b3a4495ea08e2edbb3
Richard Snyder <firstwatchwebs@hotmail.com>
2020-07-07 16:35:52 -0500

    Change who we say hello to

--- src/com/first/team123/Main.java
+++ src/com/first/team123/Main.java
@@ -2,6 +2,7 @@ package com.first.team123;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        System.out.println("Hello Rich!");
     }
 }
